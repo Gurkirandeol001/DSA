8032dc12464d5e66f015acc33372fb2ca6d26766
Gurkirandeol001 <168002614+Gurkirandeol001@users.noreply.github.com>
2026-09-25 16:09:23 +1000

    Time: 50 ms (5.76%) | Memory: 46.9 MB (74.08%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int sum=0;
         int result[]=new int[2];
         for(int i=0;i<nums.length-1;i++)
         {
@@ -7,11 +8,12 @@ class Solution {
             {
                 if(nums[i]+nums[j]==target)
                 {
-                   result[0]=i;
-                   result[1]=j;
-                   return result;
+                    result[0]=i;
+                    result[1]=j;
+                    
                 }
             }
+
         }
         return result;
     }
